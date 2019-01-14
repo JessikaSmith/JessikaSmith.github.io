23c978aa65257d3cabe83a76fba036bb7ae34586
JessikaSmith <mariyaxod@yandex.ru>
2019-01-15 00:44:47 +0300

    lab2 table load

---
+++ hw2/js/table_script.js
@@ -0,0 +1,115 @@
+function f() {
+    var entries = arguments;
+    var i = 0;
+    var l = entries.length;
+    while (i < l) {
+        if (typeof(entries[i]) === 'string' || typeof(entries[i]) === 'number') {
+            entries[i] = (function (str) {
+                return function (d) {
+                    return d[str];
+                };
+            })(entries[i]);
+        }
+        i++;
+    }
+    return function (d) {
+        var i = 0;
+        l = entries.length;
+        while (i++ < l) d = entries[i - 1].call(this, d);
+        return d;
+    };
+}
+
+// defining columns and format
+const columns = [
+    {head: 'Name', cl: 'title', html: f('Name')},
+    {head: 'Continent', cl: 'center', html: f('Continent')},
+    {head: 'GDP', cl: 'num', html: f('GDP', d3.format(',.3s'))},
+    {head: 'Life Expectancy', cl: 'center', html: f('Life Expectancy', d3.format('.1f'))},
+    {head: 'Population', cl: 'num', html: f('Population', d3.format(',.0f'))},
+    {head: 'Year', cl: 'center', html: f('Year', d3.format('.0f'))}
+];
+
+
+function prepare_columns(columns){
+  return columns.map(function (t){
+    return{
+      'Name': t.name,
+      'Continent': t.continent,
+      'GDP': t.gdp,
+      'Life Expectancy': t.life_expectancy,
+      'Population': t.population,
+      'Year': t.year
+      };
+    });
+  }
+
+function table_show(data){
+
+  var sortAscending = true;
+  var table = d3.select("body").append("table"),
+  thead = table.append("thead")
+              .attr("class", "thead");
+  tbody = table.append("tbody");
+
+  table.append("caption")
+              .html("World Countries Ranking");
+
+  const headers = thead.append("tr").selectAll("th")
+    .data(columns)
+    .enter()
+    .append("th")
+    .text(function(d) {
+      return d.head;
+    })
+    .on("click", function(header, d) {
+
+      tbody.selectAll("tr").sort(function(a, b) {
+        return d3.descending(a[header], b[header]);
+      });
+
+      if (sortAscending){
+         rows.sort(function(a, b) { return b[d] < a[d]; });
+         sortAscending = false;
+         this.className = 'aes';
+       } else {
+    	 rows.sort(function(a, b) { return b[d] > a[d]; });
+    	 sortAscending = true;
+    	 this.className = 'des';
+       }
+
+    });
+
+    var rows = tbody.selectAll("tr.row")
+        .data(data)
+        .enter()
+        .append("tr").attr("class", "row");
+
+
+    // var rows = tbody.selectAll("tr.row")
+    //     .data(data)
+    //     .enter()
+    //     .append("tr").attr("class", "row");
+
+    var cells = rows.selectAll('td')
+        .data(function(row, i) {
+          return columns.map(function (c) {
+            const cell = {};
+            d3.keys(c).forEach(function (k) {
+              cell[k] = typeof c[k] == 'function' ? c[k](row, i) : c[k];
+            });
+            return cell;
+          });
+        })
+        .enter()
+        .append('td')
+        .html(f('html'))
+        .attr('class', f('cl'));
+    const t1 = tbody.selectAll('tr.row').selectAll('td');
+
+  };
+
+
+d3.json("http://localhost:8000/json_files/countries_2012.json", function(error, data){
+    table_show(prepare_columns(data));
+  });
